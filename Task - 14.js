b87c889a868e464437c4a122bca12a17c733b7bf
Gokul <smgokul00@gmail.com>
2026-04-21 11:22:57 +0530

    First upload

---
+++ Task - 14.js
@@ -0,0 +1,5 @@
+function change() {
+    document.getElementById("text").innerHTML = "Hii!";
+    document.getElementById("text").style.color = "red";
+    document.getElementById("text").style.fontStyle = "italic";
+}
